0ea926d7dd091c5e9ff89e3a817a82587e99b5e2
Mason Fischer <mason@thoughtbot.com>
2015-09-13 11:40:36 -0500

    Clicks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+var data = ["mason"]
 
-export default class App extends Component {
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {count: 0};
+  }
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
     );
   }
 }
